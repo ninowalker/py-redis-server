1f491fcf2959ce7dab766f8b470ace7d27edda14
Nino Walker <nino@livefyre.com>
2013-09-02 11:13:30 -0700

    Updated examples.

--- examples/simple.py
+++ examples/simple.py
@@ -2,6 +2,7 @@ import sys
 import os
 import glob
 import asyncore
+from collections import defaultdict
 
 # make demos easy.
 __parent = os.path.join(os.path.dirname(__file__), "../")
@@ -10,11 +11,19 @@ sys.path.extend(glob.glob(os.path.join(__parent, "*.egg")))
 
 from rediserver.net import AsyncoreServer
 
-_counter = iter(xrange(1000000000))
+_counter = iter(xrange(1000000000)) # used by the count module
+_incr = defaultdict(int)
 
 
-def echo(cmd, response, _handler):
-    """Echo back input from the client."""
+def echo(cmd, response, request):
+    """Echo back input from the client.
+    `cmd`: an array of strings that constitute the command, e.g. ['incr', 'foo', '1']
+    `response`: a handle for responding. Supports:
+       .encode(list|tuple|bool|int|long|string)
+       .status(msg="OK")
+       .error(msg)
+    `request`: the connection handler, generally unused.
+    """
     response.encode(cmd)
 
 
@@ -29,16 +38,21 @@ def summer(cmd, response, _handler):
     response.encode(sum(map(int, cmd)))
 
 
-MODE = {'echo': echo, 'count': count, 'sum': summer}
+def incr(cmd, response, request):
+    v = _incr[cmd[1]]
+    _incr[cmd[1]] += int(cmd[2])
+    response.encode(v)
+
+MODE = {'echo': echo, 'count': count, 'sum': summer, 'incr': incr}
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print "usage: python %s <port> <echo|count|sum>"
+        print "usage: python %s <port> [<echo|count|sum|incr>]"
         sys.exit(1)
     port, mode = sys.argv[1:]
-    mode = MODE.get(sys.argv[2])
+    mode = MODE.get(sys.argv[2], 'echo')
     if not mode:
-        print "usage: python %s <port> <echo|count|sum>"
+        print "usage: python %s <port> <echo|count|sum|incr>"
         sys.exit(1)
 
     s = AsyncoreServer('', int(port), callback=mode)
